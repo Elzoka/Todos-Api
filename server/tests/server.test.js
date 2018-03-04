97f3970fdf389ebfbb2915ca9afde9c46f1bcd71
Elzoka <mahmoud_elzoka@yahoo.com>
2018-03-04 15:06:27 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,11 +3,15 @@ const request = require("supertest");
 const app = require("./../server");
 const Todo = require("./../models/todo");
 
+const todos = [
+  {text: "Feed the cat"},
+  {text: "Study harder"}
+]
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    done()
-  }).catch(e => done(e))
+    Todo.insertMany(todos)
+  }).then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -24,7 +28,7 @@ describe("POST /todos", () => {
         if(err) {
           return done(err);
         }
-        Todo.find({}).then(todos => {
+        Todo.find({text}).then(todos => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -42,11 +46,23 @@ describe("POST /todos", () => {
           return done(err);
         }
         Todo.find({}).then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch(e => {
           done(e);
         });
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("should return a json of todos", (done) => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done)
   });
 });
