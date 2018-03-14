b8d1603cd6b20a5d5ff1ba132a29b5d2a4b4cba3
Elzoka <mahmoud_elzoka@yahoo.com>
2018-03-14 16:12:21 +0200

    fix small error

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ if(env === 'developement' || env === 'test'){
   var config = require("./config.json");
   var envConfig = config[env];
 
-  Object.keys(envConfig)forEach((key) => {
+  Object.keys(envConfig).forEach((key) => {
       process.env[key] = envConfig[key];
   });
 }
