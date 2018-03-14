ba619fa064de7b9359d3710e0828edd98d416144
Elzoka <mahmoud_elzoka@yahoo.com>
2018-03-14 17:49:35 +0200

    clean up

--- server/config/config.js
+++ server/config/config.js
@@ -7,11 +7,3 @@ if(env === 'developement' || env === 'test'){
       process.env[key] = envConfig[key];
   });
 }
-
-// if(env === 'developement'){
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-// }else if (env === 'test') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
-// }
